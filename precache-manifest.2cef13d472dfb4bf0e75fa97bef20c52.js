9b6814b99b045d65f661016db807acee73d28bf4
LinorDolev <32033925+LinorDolev@users.noreply.github.com>
2020-05-20 14:09:17 +0300

    Updates

---
+++ precache-manifest.2cef13d472dfb4bf0e75fa97bef20c52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "351e767a613f7f7cafd7d03b4586c89d",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "024f4e803173a2d608c9",
+    "url": "/weather-forecast/static/css/2.fa3fb99f.chunk.css"
+  },
+  {
+    "revision": "72f6b3666b7896bea3ac",
+    "url": "/weather-forecast/static/css/main.02fd92d2.chunk.css"
+  },
+  {
+    "revision": "024f4e803173a2d608c9",
+    "url": "/weather-forecast/static/js/2.37d72e40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-forecast/static/js/2.37d72e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f6b3666b7896bea3ac",
+    "url": "/weather-forecast/static/js/main.f91ed142.chunk.js"
+  },
+  {
+    "revision": "289cf51c6cebaee58579",
+    "url": "/weather-forecast/static/js/runtime-main.6d3dea12.js"
+  }
+]);
